6f034d349df5cc37677fb9b756e77e71e98c8ce3
natebot13 <tekkbot@gmail.com>
2014-10-18 17:01:12 -0700

    all tabs are now spaces

--- game.py
+++ game.py
@@ -4,29 +4,29 @@ from Components import *
 import pygame
 
 def main():
-	WIDTH = 512
-	HEIGHT = 512
-	SIZE = (WIDTH,HEIGHT)
-	FPS = 60
-	SCREEN = pygame.display.set_mode(SIZE)
-	pygame.display.set_caption("Hack61A")
-	CLOCK = pygame.time.Clock()
+    WIDTH = 512
+    HEIGHT = 512
+    SIZE = (WIDTH,HEIGHT)
+    FPS = 60
+    SCREEN = pygame.display.set_mode(SIZE)
+    pygame.display.set_caption("Hack61A")
+    CLOCK = pygame.time.Clock()
 
-	entManager = EntityManager()
-	sysManager = SystemsManager(entManager, SCREEN)
-	ID = entManager.newEntity()
-	entManager.addComponentToEntity(ID, Position())
-	entManager.addComponentToEntity(ID, Velocity())
-	entManager.addComponentToEntity(ID, Input())
-	entManager.addComponentToEntity(ID, Renderable('ball.png'))
-	ID = entManager.newEntity()
-	entManager.addComponentToEntity(ID, Position())
+    entManager = EntityManager()
+    sysManager = SystemsManager(entManager, SCREEN)
+    ID = entManager.newEntity()
+    entManager.addComponentToEntity(ID, Position())
+    entManager.addComponentToEntity(ID, Velocity())
+    entManager.addComponentToEntity(ID, Input())
+    entManager.addComponentToEntity(ID, Renderable('ball.png'))
+    ID = entManager.newEntity()
+    entManager.addComponentToEntity(ID, Position())
 
-	while True:
-		SCREEN.fill((0,0,0))
-	    sysManager.runSystems()
-	    pygame.display.update()
-	    CLOCK.tick(FPS)
+    while True:
+        SCREEN.fill((0,0,0))
+        sysManager.runSystems()
+        pygame.display.update()
+        CLOCK.tick(FPS)
 
 if __name__ == "__main__":
-	main()
+    main()
